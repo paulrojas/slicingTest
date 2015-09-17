8836a30c3b3afc2bda66e3c8bcf86e3ce972b303
Paul Rojas <paulrojas@iMac-de-Paul.local>
2015-09-17 10:25:36 -0500

    PJRR 20150918 1025 test completed

--- js/menu.js
+++ js/menu.js
@@ -26,57 +26,58 @@ var menu = {
         {
             label: "Filmy",
             submenu: [
-                {label:"item 1", url:"#"},
-                {label:"item 2", url:"#"}
+                {label:"item 2-1", url:"#"},
+                {label:"item 2-2", url:"#"}
             ]
         },
         {
             label: "Muzyka",
             submenu: [
-                {label:"item 1", url:"#"},
-                {label:"item 2", url:"#"}
+                {label:"item 3-1", url:"#"},
+                {label:"item 3-2", url:"#"},
+                {label:"item 3-3", url:"#"}
             ]
         },
         {
             label: "Zdjęcia",
             submenu: [
-                {label:"item 1", url:"#"},
-                {label:"item 2", url:"#"}
+                {label:"item 4-1", url:"#"},
+                {label:"item 4-2", url:"#"}
             ]
         },
         {
             label: "Gry",
             submenu: [
-                {label:"item 1", url:"#"},
-                {label:"item 2", url:"#"}
+                {label:"item 5-1", url:"#"},
+                {label:"item 5-2", url:"#"}
             ]
         },
         {
             label: "Blogy",
             submenu: [
-                {label:"item 1", url:"#"},
-                {label:"item 2", url:"#"}
+                {label:"item 6-1", url:"#"},
+                {label:"item 6-2", url:"#"}
             ]
         },
         {
             label: "Groupy",
             submenu: [
-                {label:"item 1", url:"#"},
-                {label:"item 2", url:"#"}
+                {label:"item 7-1", url:"#"},
+                {label:"item 7-2", url:"#"}
             ]
         },
         {
             label: "Forum",
             submenu: [
-                {label:"item 1", url:"#"},
-                {label:"item 2", url:"#"}
+                {label:"item 8-1", url:"#"},
+                {label:"item 8-2", url:"#"}
             ]
         },
         {
             label: "Chat",
             submenu: [
-                {label:"item 1", url:"#"},
-                {label:"item 2", url:"#"}
+                {label:"item 9-1", url:"#"},
+                {label:"item 9-2", url:"#"}
             ]
         }
     ]
